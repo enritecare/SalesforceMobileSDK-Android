8b4c2411711fcdc24c7dc914461a8a7c1808cbe9
Yash Shelatkar <yashshelatkar@Yashs-MacBook-Pro.local>
2021-10-25 08:19:03 +1030

    Reverted back to SF version 8.1.0

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
@@ -115,7 +115,7 @@ public class SalesforceSDKManager implements LifecycleObserver {
     /**
      * Current version of this SDK.
      */
-    public static final String SDK_VERSION = "9.2.0";
+    public static final String SDK_VERSION = "8.1.0.dev";
 
     /**
      * Intent action meant for instances of SalesforceSDKManager residing in other processes
